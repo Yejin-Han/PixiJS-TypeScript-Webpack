c1a50599b43b48ca314919be692ee0f68e5d3212
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2023-11-20 14:16:13 +0900

    Graphics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Let's Start");
+import { Application, Graphics } from "pixi.js";
+
+const app = new Application({
+  width: innerWidth,
+  height: innerHeight,
+});
+
+document.body.appendChild(app.view);
+
+// Graphics
+const graphic = new Graphics();
+graphic.beginFill(0xffffff); // 1st step
+graphic.drawRect(20, 50, 100, 80); // 2nd step
+graphic.endFill(); // 3rd step
+
+// Another graphic
+const graphic2 = new Graphics();
+graphic2.beginFill(0xff0000);
+graphic2.drawCircle(50, 100, 30);
+graphic2.endFill();
+
+app.stage.addChild(graphic);
+app.stage.addChild(graphic2);
